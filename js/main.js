70dae07da1fa3fc949fcb0cec958a1026e134579
Ashton <ashtonenglish3@gmail.com>
2018-12-03 12:31:16 +0200

    added a project section

--- js/main.js
+++ js/main.js
@@ -16,3 +16,18 @@ function animate($slides){
 }
 
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
